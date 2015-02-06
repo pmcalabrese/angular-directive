6eed8f1cfc8ecdc67d9ad83e6def1bccf4f35208
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2015-02-06 11:15:14 +0100

    add ng-isotope
    added throuw the browser so i need to update the documentation

--- src/directives.js
+++ src/directives.js
@@ -16,4 +16,72 @@ directive('svgLoadingspin', [function () {
     restrict: 'E',
     replace: true
   };
+}])
+/**
+ * ## Documantation for directives
+ * ### isotopeNg
+ * Dircetive that will sort a list (collection) based on the data-order attribute
+ *
+ * #### Example:
+ *	<div isotope-ng isotope-list="list"...
+ *    <div data-order="{{item.order}}" ng-repeat="item in list track by item.name">
+ * 
+ *	
+ */
+.directive('isotopeNg', ['$timeout',function($timeout) {
+    return {
+        restrict: 'A',
+        scope: {
+            isotopeList: "=isotopeList" // focusWhenDelay is necessary if you want to set a delay
+        },
+        link: function(scope, element, attrs, ngModel) {
+
+          var resultBox = angular.element(element);
+
+          function isotopeInit(resultBox) {
+            if(!resultBox.hasClass('isotope')){
+              resultBox.isotope({
+                animationEngine: 'best-available',
+                itemSelector:'.result-row',
+                containerStyleObject: {
+                  position: 'relative',
+                  overflow: 'hidden'
+                },
+                layoutMode: 'straightDown',
+                getSortData: {
+                  order : function($elem) {
+                    return parseInt($elem.attr("data-order"),10);
+                  },
+                  // mo : function($elem) {
+                  //  return parseInt($elem.attr("data-mo"),10);
+                  // },
+                  // category : function($elem) {
+                  //   return parseInt($elem.attr("data-category"),10);
+                  // },
+                  // price : function($elem) {
+                  //   return parseInt($elem.attr("data-price"),10);
+                  // }
+                }
+              });
+            }
+            resultBox.isotope();
+          }
+
+          function isotopeSort(resultBox) {
+            if (typeof resultBox !== "undefined") {
+              $timeout(function() {
+                resultBox.isotope('reloadItems');
+                resultBox.isotope({ sortBy : ['order']});
+              },100);
+            }
+          }
+
+          scope.$watch('isotopeList',function(value) {
+            isotopeSort(resultBox);
+          });
+
+          isotopeInit(resultBox);
+
+        }
+    };
 }]);
